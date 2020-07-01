c79c82691dfb54cd936d3b611054c309e7b2262a
tashfeenrao <raotashfeen25@gmail.com>
2020-07-02 01:06:06 +0500

    initialized create store factory function

---
+++ index.js
@@ -0,0 +1,18 @@
+const createStore = () => {
+    let state;
+    let listeners = [];
+
+    const getState = () => state;
+
+    const subscribe = (listener) => {
+        listeners.push(listener);
+
+        return () => {
+            listeners = listeners.filter((l) => l !== listener )
+        }
+    }
+
+    return {
+        getState, subscribe
+    }
+}
