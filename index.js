1712779b01344526fbee56e7ff090d22c6985bfe
tashfeenrao <raotashfeen25@gmail.com>
2020-07-03 01:51:39 +0500

    added UI interaction with create store function

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const TOGGLE_TODO = 'TOGGLE_TODO';
 const ADD_GOALS = 'ADD_GOALS';
 const REMOVE_GOALS = 'REMOVE_GOALS';
 
+const generateId = () => {
+    return Math.random().toString(36).substring(2) + (new Date()).getTime().toString(36);
+  }
 const addTodoCreater = (todo) => {
     return {
         type: ADD_TODO,
@@ -101,42 +104,65 @@ store.subscribe(() => {
     console.log("This is todos state", store.getState())
 })
 
-store.dispatch(addTodoCreater({
-    id: 0,
-    name: 'learn Redux Tashfeen',
-    complete: false
-}))
-store.dispatch(addTodoCreater({
-    id: 1,
-    name: 'learn Redux Tashfeen',
-    complete: false
-}))
-store.dispatch(addTodoCreater({
-    id: 2,
-    name: 'learn Redux Tashfeen',
-    complete: false
-}))
-store.dispatch(addTodoCreater({
-    id: 3,
-    name: 'learn Redux Tashfeen',
-    complete: false
-}))
-store.dispatch(addTodoCreater(1))
-store.dispatch(addTodoCreater(2))
-store.dispatch(addTodoCreater({
-    id: 1,
-    name: 'learn Redux Tashfeen',
-    complete: true
-}))
-store.dispatch(addGoalCreater({
-    id: 0,
-    name: 'Make Goals Tashfeen',
-}))
-store.dispatch(addGoalCreater({
-    id: 1,
-    name: 'Make Goals Tashfeen',
-}))
-store.dispatch(addGoalCreater({
-    id: 2,
-    name: 'Make Goals Tashfeen',
-}))
+const addTodo = () => {
+    const input = document.getElementById('todo');
+    const name = input.value;
+    input.value = '';
+
+    store.dispatch(addTodoCreater({
+        id: generateId(),
+        name,
+        complete: false
+    }))
+}
+const addGoal = () => {
+    const input = document.getElementById('goal');
+    const name = input.value;
+    input.value = '';
+
+    store.dispatch(addGoalCreater({
+        id: generateId(),
+        name,
+    }))
+}
+document.getElementById('todoBtn').addEventListener('click',addTodo);
+document.getElementById('goalBtn').addEventListener('click',addGoal);
+// store.dispatch(addTodoCreater({
+//     id: 0,
+//     name: 'learn Redux Tashfeen',
+//     complete: false
+// }))
+// store.dispatch(addTodoCreater({
+//     id: 1,
+//     name: 'learn Redux Tashfeen',
+//     complete: false
+// }))
+// store.dispatch(addTodoCreater({
+//     id: 2,
+//     name: 'learn Redux Tashfeen',
+//     complete: false
+// }))
+// store.dispatch(addTodoCreater({
+//     id: 3,
+//     name: 'learn Redux Tashfeen',
+//     complete: false
+// }))
+// store.dispatch(addTodoCreater(1))
+// store.dispatch(addTodoCreater(2))
+// store.dispatch(addTodoCreater({
+//     id: 1,
+//     name: 'learn Redux Tashfeen',
+//     complete: true
+// }))
+// store.dispatch(addGoalCreater({
+//     id: 0,
+//     name: 'Make Goals Tashfeen',
+// }))
+// store.dispatch(addGoalCreater({
+//     id: 1,
+//     name: 'Make Goals Tashfeen',
+// }))
+// store.dispatch(addGoalCreater({
+//     id: 2,
+//     name: 'Make Goals Tashfeen',
+// }))
