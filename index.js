cefa24c7448ae47eb3b9d1b345bbca6bea8a7577
tashfeenrao <raotashfeen25@gmail.com>
2020-07-02 23:42:12 +0500

    added second reducer goals

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const todos = (state = [], action) => {
-    if (action.type === 'ADD_TODO') {
-        return state.concat([action.todo])
+
+    switch(action.type) {
+        case 'ADD_TODO':
+            return state.concat([action.todo])
+        case 'REMOVE_TODO':
+            return state.filter((todo) => todo.id !== action.id)
+        case 'TOGGLE_TODO':
+            return state.map((todo) => todo.id !== action.id ? todo :
+            Object.assign({}, todo, {complete: !todo.complete}))
+        default:
+            return state;
     }
-    return state;
+}
+
+const goals = (state = [], action) => {
+    switch(action.type) {
+        case 'ADD_GOALS':
+            return state.concat([action.goals])
+        case 'REMOVE_GOALS':
+            return state.filter((goal) => goal.id !== action.id )
+        default:
+            return state;
+    }
 }
 
 
-const createStore = () => {
+const createStore = (reducer) => {
     let state;
     let listeners = [];
 
@@ -21,7 +40,7 @@ const createStore = () => {
     }
 
     const dispatch = (action) => {
-        state = todos(state,action);
+        state = reducer(state,action);
         listeners.forEach((listener) => listener())
     }
 
@@ -29,3 +48,18 @@ const createStore = () => {
         getState, subscribe, dispatch
     }
 }
+
+const store = createStore(todos)
+
+store.subscribe(() => {
+    console.log("This is todos state", store.getState())
+})
+
+store.dispatch({
+    type: 'ADD_TODO',
+    todo: {
+        id: 0,
+        name: 'learn Redux Tashfeen',
+        complete: false
+    }
+})
