4ac338b63751d5d64d07ea1265d300cd29b044c8
tashfeen rao <tashfeen@ricult.com>
2021-08-18 22:23:51 +0500

    added first phase of formik values

--- src/formik/Form.js
+++ src/formik/Form.js
@@ -1,15 +1,44 @@
 import React from "react";
 import "./form.css";
+import { useFormik } from "formik";
 
 export default function Form() {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  console.log(formik.values);
+
   return (
-    <div class='login-page'>
-      <div class='form'>
-        <form class='register-form'>
-          <input type='text' placeholder='name' />
-          <input type='password' placeholder='password' />
-          <input type='text' placeholder='email address' />
-          <button class='create'>Sign up</button>
+    <div className='login-page'>
+      <div className='form'>
+        <form className='register-form'>
+          <input
+            type='text'
+            placeholder='name'
+            name='name'
+            onChange={formik.handleChange}
+            value={formik.values.name}
+          />
+          <input
+            type='password'
+            placeholder='password'
+            name='password'
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
+          <input
+            type='text'
+            placeholder='email address'
+            name='email'
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          <button className='create'>Sign up</button>
         </form>
       </div>
     </div>
